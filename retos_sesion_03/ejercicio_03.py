026aadb7468af65434ab3cd8e260883792e61408
Adriana Quispe <adrianaquispe229@gmail.com>
2025-05-03 22:24:42 -0400

    Como lavar un plato

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,15 @@
+# ejercicio_03.py
+print("Tutorial: Cómo lavar los platos paso a paso")
+
+print("1. Junta todos los platos sucios en el lavdero.")
+print("2. Retira los restos de comida con una servilleta o en el bote de basura o se lo das perrito.")
+print("3. Usamos ola y dejamos que remoje un momentos, uno 10 min.")
+print("4. Usa una esponja suave (amarilla-verde)")
+print("5. Comienza lavando los cubiertos.")
+print("6. Después, lava los platos.")
+print("7. Enjuaga cada elemento con agua limpia después de enjabonarlo.")
+print("8. Coloca los platos en un escurridor para que se sequen al aire.")
+print("9. Si lo prefieres, puedes secarlos con un trapo limpio.")
+print("10. Limpia el fregadero y guardamos los platos.")
+
+print("¡Platos limpios!")
