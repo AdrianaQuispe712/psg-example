68bb8fa306b87316705dc438672dbe88990e046d
Adriana Quispe <adrianaquispe229@gmail.com>
2025-05-24 15:09:31 -0400

    sesion 5

---
+++ sesion05.py
@@ -0,0 +1,17 @@
+
+minutos = 300
+segundos = 3600
+minutos_extra = segundos / 60
+minutos_totales = minutos + minutos_extra
+horas_totales = minutos_totales / 60
+print("total de", horas_totales, "horas.")
+
+
+print ("Operadores de comparación")
+comparar = 10
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
