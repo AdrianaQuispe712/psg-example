350df43e6be9c95d08b8b6a60007ac5b8dba8f28
Adriana Quispe <adrianaquispe229@gmail.com>
2025-05-31 15:07:14 -0400

    Ejercicio 02: Operador XNOR en Python

---
+++ retos_sesion_06/ejercicio_02.py
@@ -0,0 +1,8 @@
+def xnor(a, b):
+    return not (a ^ b) 
+
+# Ejemplo de uso:
+print(xnor(True, True))   # True
+print(xnor(True, False))  # False
+print(xnor(False, True))  # False
+print(xnor(False, False)) # True
