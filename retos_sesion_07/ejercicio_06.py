9b6a7e7a8b0acbc75902f33035021ae8b91f6774
Adriana Quispe <adrianaquispe229@gmail.com>
2025-06-07 13:07:04 -0400

    nuevas funciones

---
+++ retos_sesion_07/ejercicio_06.py
@@ -0,0 +1,19 @@
+# 1. endswith() – Verifica si termina con cierto texto
+archivo = "reporte_final.txt"
+print(archivo.endswith(".txt"))
+
+# 2. zfill() – Rellenar con ceros a la izquierda
+codigo = "23"
+print(codigo.zfill(5))  # Resultado: '00023'
+
+# 3. center() – Centra una cadena en un ancho específico
+titulo = "Python"
+print(titulo.center(20, "-"))
+
+# 4. ljust() – Justifica a la izquierda
+nombre = "Ana"
+print(nombre.ljust(10, "."))  # Resultado: 'Ana.......'
+
+# 5. partition() – Divide en una tupla en torno a un separador
+mensaje = "Hola mundo bonito"
+print(mensaje.partition("mundo"))  # ('Hola ', 'mundo', ' bonito')
