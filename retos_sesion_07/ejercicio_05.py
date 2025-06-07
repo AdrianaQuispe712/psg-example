7ebe6cf4f27f00fb89dc8b60e84aac282ec8c4eb
Adriana Quispe <adrianaquispe229@gmail.com>
2025-06-07 13:05:58 -0400

    Funciones de búsqueda y validación en cadenas

---
+++ retos_sesion_07/ejercicio_05.py
@@ -0,0 +1,7 @@
+def es_palindromo(texto):
+    texto = texto.lower()
+    texto = texto.replace(" ", "")
+    return texto == texto[::-1]
+
+frase = input("Ingrese una frase: ")
+print("Es palindromo?", es_palindromo(frase))
